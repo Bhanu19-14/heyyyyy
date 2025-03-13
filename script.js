d75ff45d82f4c08611dd6b2e8d07376b2b380119
Bhanu19-14 <bp2829666@gmail.com>
2025-03-13 21:36:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function celebrate() {
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+    alert("Happy Birthday beautiful! 🎉 You are the sweetest! 💖");
+}
